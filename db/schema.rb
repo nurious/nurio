5002def8c4dc861482899d620eb4e8d0e217e42e
Todd Metheny <todd.metheny@gmail.com>
2014-12-23 13:15:41 -0500

    created an organization scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141223180724) do
+
+  create_table "organizations", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
