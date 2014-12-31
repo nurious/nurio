e4f17042581b417bd42f2b1e4eac9a0705e5f65d
Todd Metheny <todd.metheny@gmail.com>
2014-12-31 17:06:29 -0500

    changed the delay from 1000 to 800

--- app/assets/javascripts/organizations.js
+++ app/assets/javascripts/organizations.js
@@ -1,5 +1,5 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function(){
-	$("#notice").delay(1000).fadeOut();
+	$("#notice").delay(800).fadeOut();
 })
