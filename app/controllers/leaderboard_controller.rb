d62e95cae71cb5dfa96466e00cd1f6ba4376af57
Todd Metheny <todd.metheny@gmail.com>
2015-01-01 17:12:08 -0500

    fixed leaderboard error for non-signed in user. created redirect to login unless user is logged in

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -1,7 +1,10 @@
 class LeaderboardController < ApplicationController
   def show
-  	org = current_user.organization
-  	@users = org.users.all.sort{ |x, y| y.score <=> x.score }
-  	# @users = Organization.users.all.sort{ |x, y| y.score <=> x.score }
+  	if current_user
+	  	org = current_user.organization
+	  	@users = org.users.all.sort{ |x, y| y.score <=> x.score }
+  	else
+  		redirect_to new_user_registration_path
+  	end
   end
 end
