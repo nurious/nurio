26aa37fcf39718f23dc129f7e7ffce2a15396e95
Todd Metheny <todd.metheny@gmail.com>
2015-01-01 16:47:48 -0500

    trying to limit leaderboard to the current users org

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -1,5 +1,6 @@
 class LeaderboardController < ApplicationController
   def show
   	@users = User.all.sort{ |x, y| y.score <=> x.score }
+  	# @users = Organization.users.all.sort{ |x, y| y.score <=> x.score }
   end
 end
