4a036a5dcfa1b9218836056099ebcbf0efda56a7
Ray Braaf <braaf.raymond@gmail.com>
2015-01-01 21:15:41 -0500

    migration change

--- db/schema.rb
+++ db/schema.rb
@@ -62,11 +62,8 @@ ActiveRecord::Schema.define(version: 20150101232035) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
     t.string   "img_url"
   end
-
-  add_index "organizations", ["user_id"], name: "index_organizations_on_user_id"
 
   create_table "positions", force: true do |t|
     t.string   "name"
@@ -78,16 +75,6 @@ ActiveRecord::Schema.define(version: 20150101232035) do
 
   add_index "positions", ["department_id"], name: "index_positions_on_department_id"
   add_index "positions", ["organization_id"], name: "index_positions_on_organization_id"
-
-  create_table "reputations", force: true do |t|
-    t.integer  "score"
-    t.integer  "rank"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "reputations", ["user_id"], name: "index_reputations_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -109,16 +96,5 @@ ActiveRecord::Schema.define(version: 20150101232035) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["organization_id"], name: "index_users_on_organization_id"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
-  create_table "versions", force: true do |t|
-    t.string   "item_type",  null: false
-    t.integer  "item_id",    null: false
-    t.string   "event",      null: false
-    t.string   "whodunnit"
-    t.text     "object"
-    t.datetime "created_at"
-  end
-
-  add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
 
 end
